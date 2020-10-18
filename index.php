51a1f066391f4aa4fe28aac329d060f105addbf4
FelixJungensbluth <felix.jungensbluth@stud.hshl.de>
2020-10-18 15:54:26 +0200

    index.php

--- index.php
+++ index.php
@@ -8,8 +8,8 @@
     <script>
     var config = {
         type: Phaser.AUTO,
-        width: 800,
-        height: 600,
+        width: 1280,
+        height: 720,
         physics: {
             default: 'arcade',
             arcade: {
@@ -31,7 +31,7 @@
         this.load.image('sky', 'assets/skies/space3.png');
         this.load.image('logo', 'assets/sprites/phaser3-logo.png');
         this.load.image('red', 'assets/particles/red.png');
-        
+
     }
 
     function create ()
